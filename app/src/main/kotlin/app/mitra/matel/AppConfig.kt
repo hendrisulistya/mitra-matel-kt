295897171619becf6edede8994e954ac2317543d
Hendri Sulistya <id.hendrisulistya@gmail.com>
2025-10-26 02:09:01 +0700

    update cert pinning

--- app/src/main/kotlin/app/mitra/matel/AppConfig.kt
+++ app/src/main/kotlin/app/mitra/matel/AppConfig.kt
@@ -35,6 +35,6 @@ object AppConfig {
         return if (IS_PRODUCTION) "Production" else "Development"
     }
     fun getAppVersion(): String {
-        return "0.9.114 (Build 1212)"+if (IS_PRODUCTION) "-prod" else "-debug"
+        return "0.9.120 (Build 1240)"+if (IS_PRODUCTION) "-prod" else "-debug"
     }
 }
